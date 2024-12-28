06597c7968644f283a82387d18cd6f550a40d121
jeter <a0958672769@gmail.com>
2024-12-29 01:47:26 +0800

    [root] 頁面刷新404問題

--- backend/src/repository/report/ReportRepository.ts
+++ backend/src/repository/report/ReportRepository.ts
@@ -12,42 +12,33 @@ export class ReportRepository implements IReportRepository {
 
     async daily(): Promise<any[]> {
         const query = `
-            WITH staffPay AS (SELECT -SUM(sw.pay) AS totalSales,
-                                     '員工薪水'  AS name,
-                                     0           AS id,
-                                     2           AS salesType
-                              FROM GuiGeDb.StaffWork AS sw
-                                       LEFT JOIN
-                                   GuiGeDb.Staff AS s
-                                   ON
-                                       sw.staffId = s.id
-                              WHERE DATE(sw.createdAt) = CURDATE()
-                                AND sw.deletedAt IS NULL
-                                AND s.deletedAt IS NULL
-                              GROUP BY s.id),
-
-                 dailyTurnover AS (SELECT SUM(IF(ds.salesType = 1, ds.money, 0)) -
-                                          SUM(IF(ds.salesType = 2, ds.money, 0)) AS totalSales,
-                                          dst.name,
-                                          dst.id,
-                                          ds.salesType
-                                   FROM GuiGeDb.DailySales AS ds
-                                            LEFT JOIN
-                                        GuiGeDb.DailySalesType AS dst
-                                        ON
-                                            ds.dailySalesTypeId = dst.id
-                                   WHERE DATE(ds.createdAt) = CURDATE()
-                                     AND ds.deletedAt IS NULL
-                                     AND dst.deletedAt IS NULL
-                                   GROUP BY dst.id, dst.name, ds.salesType)
-
             SELECT *
-            FROM dailyTurnover
+            FROM (
+                     SELECT SUM(IF(ds.salesType = 1, ds.money, 0)) - SUM(IF(ds.salesType = 2, ds.money, 0)) AS totalSales,
+                            dst.name,
+                            dst.id,
+                            ds.salesType
+                     FROM GuiGeDb.DailySales AS ds
+                              LEFT JOIN GuiGeDb.DailySalesType AS dst
+                                        ON ds.dailySalesTypeId = dst.id
+                     WHERE DATE(ds.createdAt) = CURDATE()
+                       AND ds.deletedAt IS NULL
+                       AND dst.deletedAt IS NULL
+                     GROUP BY dst.id, dst.name, ds.salesType
+                 ) AS dailyTurnover
 
             UNION ALL
 
-            SELECT *
-            FROM staffPay;
+            SELECT -SUM(sw.pay) AS totalSales,
+                   '員工薪水' AS name,
+                   0 AS id,
+                   2 AS salesType
+            FROM GuiGeDb.StaffWork AS sw
+                     LEFT JOIN GuiGeDb.Staff AS s
+                               ON sw.staffId = s.id
+            WHERE DATE(sw.createdAt) = CURDATE()
+              AND sw.deletedAt IS NULL
+              AND s.deletedAt IS NULL;
         `;
         return await this.queryRunner.query(query); // 返回報表的查詢結果
     }
